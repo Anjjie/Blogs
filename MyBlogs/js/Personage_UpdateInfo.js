a0938b69a5e98c992452bca516f524d37a4379ce
Anjjie <Anjjie@LAPTOP-SO7JVAVT>
2017-12-04 14:03:16 +0800

    版本：1.0.2 时间：2017.12.04 更新内容： 完成修改密码页面、修改信息页面、绑定手机号码页面布局

--- MyBlogs/js/Personage_UpdateInfo.js
+++ MyBlogs/js/Personage_UpdateInfo.js
@@ -1,1 +1,12 @@
-﻿
+﻿//设置页面中间布局高度
+function SetCenterHeight() {
+	var height = document.documentElement.clientHeight - 195;
+	var $center = $("center");
+	if(height < 500) {
+		height = 600;
+	}
+	$center.height(height);
+	window.onresize = function() {
+		SetCenterHeight();
+	}
+}
